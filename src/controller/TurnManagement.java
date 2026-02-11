9ab324f07c4801cecb2f3bf92ce59ee57e901408
sasi12k <sasi12k@email.com>
2026-02-11 15:43:06 +0700

    sasii

---
+++ src/controller/TurnManagement.java
@@ -0,0 +1,57 @@
+package controller;
+
+import gameState.GameState;
+import player.Player;
+import minnion.Minion;
+import strategy.evaluator.StrategyEvaluator;
+import strategy.evaluator.StrategyEvaluatorImpl;
+
+
+public class TurnManagement {
+
+    private GameState gameState;
+    private EndgameChecker endgameChecker;
+    private StrategyEvaluator evaluator;
+
+    public TurnManagement(GameState gameState) {
+        this.gameState = gameState;
+        this.endgameChecker = new EndgameChecker(gameState);
+        this.evaluator = new StrategyEvaluatorImpl(); //  ติดปัญหาของวุ้นเส้นค่อยแก้
+    }
+
+    /**
+     * เรียกทุกครั้งเมื่อผู้เล่นกด End Turn
+     */
+    public void endTurn() {
+        if (gameState.isGameOver()) {
+            return;
+        }
+
+        Player currentPlayer = gameState.getCurrentPlayer();
+
+        // เพิ่มงบประมาณประจำเทิร์น
+        currentPlayer.addTurnBudget(); // เรียกใช้addTurnBudget();
+
+        //  คิดดอกเบี้ย
+        currentPlayer.applyInterest(gameState.getConfig().interestPct);
+
+
+        // ให้ minion ของผู้เล่นนี้ทำงานตาม strategy
+        for (Minion minion : currentPlayer.getMinions()) {
+            if (minion.isAlive()) {
+                evaluator.evaluate(minion, gameState);// วุ้นนะะะะะะะะ
+            }
+
+        }
+
+        // เช็คเกมจบหรือยัง
+        if (endgameChecker.checkEndgame()) {
+            gameState.setGameOver(true);
+            return;
+        }
+
+        // เปลี่ยนเทิร์นไปอีกฝ่าย
+        gameState.switchTurn();
+    }
+
+}
