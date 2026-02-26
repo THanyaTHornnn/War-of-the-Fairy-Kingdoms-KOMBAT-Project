655d9a84abbc6d752da44ba31df4390b59e6d4e6
Rungphailin <inkth93@gmail.com>
2026-02-26 22:40:35 +0700

    แก้ไข bug

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -80,9 +80,6 @@ public class GameController {
 
         // ⭐ ทุกคนต้อง execute strategy
         logs = turnManager.executeStrategies();
-        for (TurnManager.MinionLog log : logs) {
-            System.out.println(log);
-        }
 
         // check end
         if (logic.checkEndGame()) {
