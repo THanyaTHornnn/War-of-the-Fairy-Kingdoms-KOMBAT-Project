7d2706705a863fa93babc1a2dc9b6f0e71b33318
Rungphailin <inkth93@gmail.com>
2026-03-23 14:28:09 +0700

    edit init budget

--- src/main/java/com/kombat/core/GameLogic.java
+++ src/main/java/com/kombat/core/GameLogic.java
@@ -51,6 +51,7 @@ public class GameLogic {
 
     public void startGame() {
         assertPhase(GameState.Phase.SETUP);
+        initBudgets();
         this.phase = GameState.Phase.PLAYING;
     }
 
